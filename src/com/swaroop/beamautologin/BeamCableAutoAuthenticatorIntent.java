9b4243c0f4faf0286207da9745505aa95ef8a74b
gnanaswaroop@gmail.com <gnanaswaroop@gmail.com@77cf014c-5cee-bec5-790d-c0655d0b0c10>
2014-07-22 03:58:35 +0000

    Added support to launch the application from Intents also.
    beamauth://login - Login action
    beamauth://logout - Logout action

---
+++ src/com/swaroop/beamautologin/BeamCableAutoAuthenticatorIntent.java
@@ -0,0 +1,19 @@
+package com.swaroop.beamautologin;
+
+import android.os.Bundle;
+
+/**
+ * Beam Cable Auto Authenticator - Intent
+ * @see BeamCableAutoAuthenticator for more details. 
+ * @author swaroop
+ *
+ */
+
+public class BeamCableAutoAuthenticatorIntent extends BeamCableAutoAuthenticator {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+
+}
